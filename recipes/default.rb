f5dc78c76b764f47be6c6f306263d8011861ee24
Eric Chen <eric.chen@americaontop.com>
2018-01-10 16:32:16 +0000

    init

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,21 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+node.default['haproxy']['members'] =
+  [
+    {
+      "hostname" => "ec2-107-21-86-131.compute-1.amazonaws.com",
+      "ipaddress" => "107.21.86.131",
+      "port" => 80,
+      "ssl_port" => 80
+    },
+    {
+      "hostname" => " ec2-54-205-195-8.compute-1.amazonaws.com",
+      "ipaddress" => "54.205.195.8",
+      "port" => 80,
+      "ssl_port" => 80
+    }
+  ]
+
+include_recipe 'haproxy::manual'
